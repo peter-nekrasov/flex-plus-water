ad73024b8e180fe3a714b1ef468ed027f95507de
Peter <peter@Peters-MacBook-Pro.local>
2024-10-02 10:56:17 -0500

    fixed greens function evaluators

--- green.m
+++ green.m
@@ -23,13 +23,11 @@ val = 0;
 for i = 1:5
     rhoj = rts2(i);
     ej = ejs(i);
-    if imag(rhoj) == 0
-        [cr0,~] = struve102(rhoj*r);
-        val = val - 1i*ej*rhoj^2*(cr0 + besselh(0, rhoj*r));
-    else
-        [ck0,~] = struveK102(-rhoj*r);
-        val = val + ej*rhoj^2*ck0;
+    [ck0] = struveK102(-rhoj*r);
+    if angle(rhoj) == 0
+        ck0 = ck0 + 4i*besselj(0, rhoj*r); % is this the right factor of J?
     end
+    val = val + ej*rhoj^2*ck0;
 end
 
 end
--- struveK102.m
+++ struveK102.m
@@ -1,29 +1,10 @@
-% function [ck0,ck1] = struveK102(z)
-% 
-%     sz = size(z);
-%     z = z(:);
-% 
-%     [ck0,ck1] = struve102(-z);
-% 
-%     h0 = besselh(0,-z);
-%     h1 = besselh(1,-z);
-% 
-%     ck0 = -1i*ck0+1i*h0;
-%     ck1 = -1i*ck1+1i*h1; % needs to be checked
-% 
-%     ck0 = reshape(ck0,sz);
-%     ck1 = reshape(ck1,sz);
-% 
-% end
-
-
 function [ck0,ck1] = struveK102(z)
 
     sz = size(z);
-    z = -z(:);
+    z = z(:);
 
     iupp = find(imag(z) >= 0);
-    ilow = find(imag(z) <= 0);
+    ilow = find(imag(z) < 0);
 
     zt = z;
     zt(ilow) = -z(ilow);
@@ -41,9 +22,10 @@ function [ck0,ck1] = struveK102(z)
     ck1(iupp) = -1i*ck1(iupp)+1i*h1(iupp);
 
     ck0(ilow) =  1i*ck0(ilow)+1i*h0(ilow);
-    ck1(ilow) = -1i*ck0(ilow)-1i*h1(ilow);
+    ck1(ilow) = -1i*ck1(ilow)-1i*h1(ilow);
 
     ck0 = reshape(ck0,sz);
     ck1 = reshape(ck1,sz);
+
 
 end
