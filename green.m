2197d0a4db6de6ba0904563ce3f89e6501923c2a
Peter <peter@Peters-MacBook-Pro.local>
2024-10-03 17:44:18 -0500

    fixed greens function evaluators

--- green.m
+++ green.m
@@ -23,9 +23,10 @@ val = 0;
 for i = 1:5
     rhoj = rts2(i);
     ej = ejs(i);
-    [ck0] = struveK102(-rhoj*r);
+    [ck0,~] = struveK102(-rhoj*r);
     if angle(rhoj) == 0
-        ck0 = ck0 + 4i*besselj(0, rhoj*r); % is this the right factor of J?
+        [cr0,~] = struve102(rhoj*r); 
+        ck0 = 1i*(cr0 + besselh(0, rhoj*r));  
     end
     val = val + pi/2*ej*rhoj^2*ck0;
 end
--- plot_green.m
+++ plot_green.m
@@ -1,10 +1,10 @@
 % Plotting green's function
 %
 
-beta = 3;
+beta = 0.5;
 gamma = -1;
 xs = 0:0.01:50*beta^(-1/4);
 
 plot(xs,real(green(xs,beta,gamma)),xs, imag(green(xs,beta,gamma)))
-legend('real','complex')
+legend('real','imaginary')
 title(['greens function, \beta = ', num2str(beta),', \gamma = ', num2str(gamma)])
