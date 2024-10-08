a09fd3c24924045dce90651e20c4721fdb9bf34f
Peter <peter@Peters-MacBook-Pro.local>
2024-10-07 22:58:29 -0500

    finished beta scattering code with error

--- scattering/scattering_variable_beta.m
+++ scattering/scattering_variable_beta.m
@@ -19,7 +19,7 @@ gamma = -1;
 k = rts((imag(rts) == 0) & (real(rts) > 0));
 
 % Perturbing coefficients
-betabar = 1*exp(-(X.^2 + Y.^2)/25); 
+betabar = 1*X.*exp(-(X.^2 + Y.^2)/25); 
 beta = beta0 + betabar;
 
 % Constructing integral operator
@@ -29,7 +29,6 @@ Gs_aug = [Gs, flip(Gs(1:end,2:end),2); ...
     flip(Gs(2:end,1:end)), flip(flip(Gs(2:end,2:end)),2)];
 Gs_aug_hat = fft2(Gs_aug)*h*h;
 
-
 % RHS (Incident field)
 phiinc = exp(1i*k*X);
 rhs = k*betabar.*phiinc;
@@ -37,7 +36,7 @@ rhs_vec = rhs(:);
 
 % Solve with GMRES
 start = tic;
-mu = gmres(@(mu) lhs(mu,betabar,Gs_aug_hat),rhs_vec,[],1e-13,200);
+mu = gmres(@(mu) lhs(mu,betabar,Gs_aug_hat),rhs_vec,[],1e-12,200);
 mu = reshape(mu, size(X));
 t1 = toc(start);
 fprintf('%5.2e s : time to solve\n',t1)
@@ -55,53 +54,91 @@ phi_n_aug = ifft2(Gs_aug_hat.*mu_aug_hat);
 phi = phi_aug(1:n,1:n);
 phi_n = phi_n_aug(1:n,1:n);
 
+phi_tot = phiinc+phi;
+phi_n_tot = phi_n + k*phiinc;
+
 tiledlayout(1,5)
 nexttile
-h = pcolor(X,Y,beta);
-h.EdgeColor = 'none';
+pc = pcolor(X,Y,beta);
+pc.EdgeColor = 'none';
 title('\beta')
 colorbar
 
 nexttile
-h = pcolor(X,Y,real(phiinc+phi));
-h.EdgeColor = 'none';
-clim([-1.5 1.5])
+pc = pcolor(X,Y,real(phi_tot));
+pc.EdgeColor = 'none';
+%clim([-1.5 1.5])
 title('Re(\phi)')
 colorbar
 
 nexttile
-h = pcolor(X,Y,abs(phiinc+phi));
-h.EdgeColor = 'none';
-clim([0 1.5])
+pc = pcolor(X,Y,abs(phi_tot));
+pc.EdgeColor = 'none';
+%clim([0 1.5])
 title('|\phi|')
 colorbar
 
 nexttile
-h = pcolor(X,Y,real(phi_n + k*phiinc));
-h.EdgeColor = 'none';
-clim([-1.5 1.5]*k)
+pc = pcolor(X,Y,real(phi_n_tot));
+pc.EdgeColor = 'none';
+%clim([-1.5 1.5]*k)
 title('real(\phi_n)')
 colorbar
 
 nexttile
-h = pcolor(X,Y,abs(phi_n + k*phiinc));
-h.EdgeColor = 'none';
-clim([0 1.5]*k)
+pc = pcolor(X,Y,abs(phi_n_tot));
+pc.EdgeColor = 'none';
+%clim([0 1.5]*k)
 title('|\phi_n|')
 colorbar
 
 rmpath('../greens function/')
 
-%% Checking error
+%% Calculating error
 
 ind = intersect(find(X == 16), find(Y == 0));
 [ii, jj] = ind2sub(size(X),ind);
 
+% finite difference stencils
+d1 = zeros(9,1);
+d1(1) = 7/240;	
+d1(2) = -2/5;
+d1(3) = 169/60;
+d1(4) = -122/15;
+d1(5) = 91/8;
+d1(6) = -122/15;
+d1(7) = 169/60;
+d1(8) = -2/5;
+d1(9) = 7/240;
 
+d2 = zeros(9, 1);
+d2(1) = -1/560;
+d2(2) = 8/315;
+d2(3) = -1/5;
+d2(4) = 8/5;
+d2(5) = -205/72;
+d2(6) = 8/5;
+d2(7) = -1/5;
+d2(8) = 8/315;
+d2(9) = -1/560;
 
+bilap = zeros(9,9);
+bilap(:,5) = d1;
+bilap(5,:) = bilap(5,:) + d1.';
+bilap = bilap + 2*(d2*d2.');
+bilap = bilap / h^4;
 
+phi_n_tot_sub = phi_n_tot(ii-4:ii+4,jj-4:jj+4);
+first = sum(bilap.*phi_n_tot_sub,'all') ;
+second = -beta(ii,jj)*phi_n_tot(ii,jj);
+third = gamma*phi_tot(ii,jj);
+err = abs(first + second + third) / max(abs(phi_n(:)))
 
-
+%phiinc_sub = phiinc(ii-4:ii+4,jj-4:jj+4);
+%first = k*sum(bilap.*phiinc_sub,'all') / h^4;
+%second = -beta0*k*phiinc(ii,jj);
+%third = gamma*phiinc(ii,jj);
+%err = first + second + third
 
 function v = lhs(mu,betabar,Gs_aug_hat)
     N = sqrt(size(mu));
