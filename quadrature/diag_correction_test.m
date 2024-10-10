a25772f41f1e0f4c4bc501071394a29326ecbdda
Peter <peter@Peters-MacBook-Pro.local>
2024-10-10 09:54:43 -0500

    quadrature

---
+++ quadrature/diag_correction_test.m
@@ -0,0 +1,46 @@
+hs = [4 2 1 0.5 0.25 0.125];
+errs = hs*0;
+xmin = -50;
+xmax = 50;
+beta = 5;
+gamma = -1;
+integrand = @(x,y) green(sqrt(x.^2 + y.^2),beta,gamma,false).*exp(-1/pi*(x.^2+y.^2));
+true_int = integral2(integrand,xmin,xmax,xmin,xmax);
+
+for ii = 1:numel(hs)
+    h = hs(ii);
+    xs = xmin:h:xmax;
+    [~,n] = size(xs);
+    [X,Y] = meshgrid(xs);
+
+    % Correction to Greens function
+    R = sqrt(X.^2 + Y.^2);
+    gval = green(R,beta,gamma,false);
+    testf = exp(-1/pi*(R.^2));
+    intmat = kernmat(@(y1,y2) green(sqrt(y1.^2 + y2.^2),beta,gamma),h,xmax,0);
+    int = sum(testf.*intmat,'all');
+    errs(ii) = abs(true_int - int);
+    disp(abs(true_int - int))
+
+    %intmat = kernmat(@(x,y) green(sqrt(x.^2+y.^2),beta,gamma),h,[xmin xmax],[xmin xmax],5);
+    %errs_w_correction(ii) = abs(true_int - int_corrected);
+end
+
+%% Plotting error
+
+loglog(hs, errs_no_correction,'o-')
+hold on
+loglog(hs, hs.^4)
+legend('1 pt stencil','h^4')
+ylabel('absolute error')
+xlabel('h')
+
+%% Plotting 
+
+loglog(hs, errs,'o-', hs, pts1_errs, 'o-', hs, pts5_errs, 'o-', hs, pts13_errs, 'o-')
+hold on
+loglog(hs,hs.^4,'--',hs,0.00005*hs.^10,'--')
+ylim([min(pts13_errs),max(pts13_errs)])
+legend('no correction', '1 pt stencil','5 pt stencil', '13 pt stencil','h^4', 'h^{10}' ,'Location','northwest')
+ylabel('relative error')
+xlabel('h')
---
+++ quadrature/kernmat.m
@@ -0,0 +1,123 @@
+function intmat = kernmat(intfun,h,ymax,nt)
+%KERNMAT forms the square matrix for a kernel using the 
+%       center-corrected trapezoid rule outlined in Duan & Rokhlin (2008).
+%
+% input:
+%
+% intfun - @(y1,y2) function handle of integral operator K(x1,x2,y1,y2) 
+%               where (x1,x2) = (0,0).
+% h - float, grid spacing 
+% ymax - float, box size with y1, y2 in [-ymax, ymax].
+%
+% optional input: 
+%
+% nt - int, number of points in the stencil. default: nt = 5. nt must be a
+%   centered square number. 
+%           - i.e. nt = 1, fourth order
+%           - nt = 5, sixth order
+%           - nt = 13, eighth order
+%           - nt = 25, tenth order, etc. 
+%
+% output: 
+%
+% - intmat is the center-corrected integral operator 
+
+
+[X, Y] = meshgrid(-ymax:h:ymax);
+[gmat] = intfun(X,Y);
+gmat(isnan(gmat)) = 0;
+
+if nargin < 4
+    nt = 5;
+elseif nt == 0
+    intmat = gmat*h^2;
+    return
+else
+    lev = 0;
+    numb = 0;
+
+    while numb < nt
+        numb = lev^2 + (lev+1)^2;
+        lev = lev + 1;
+    end
+
+    if (nt < 1) || (numb ~= nt)
+        error('nt must be a centered square number')
+    end
+end
+
+lhs = zeros(nt);
+rhs = zeros(nt,1);
+
+[xs, ys] = meshgrid(-(lev-1):(lev-1));
+rs = sqrt(xs.^2 + ys.^2);
+
+pts = sortrows([rs(:) xs(:) ys(:)]);
+iis = pts(1:nt,2);
+jjs = pts(1:nt,3);
+xpts = pts(1:nt,2)*h;
+ypts = pts(1:nt,3)*h;
+
+
+if intfun(1,1) == -intfun(-1,1)
+    xodd = 1;
+elseif intfun(1,1) == intfun(-1,1)
+    xodd = 0;
+else
+    xodd = 0.5;
+end
+
+if intfun(1,1) == -intfun(1,-1)
+    yodd = 1;
+elseif intfun(1,1) == intfun(1,-1)
+    yodd = 0;
+else
+    yodd = 0.5;
+end
+
+i = 1;
+z = 0;
+while i <= nt
+    [xpow, ypow] = cantor(z);
+    row = (xpts.^xpow).*(ypts.^ypow).*exp(-1/(25*h^2)*(xpts.^2+ypts.^2));
+    lhs(i,:) = row.';
+    if rank(lhs) == i
+        
+        if (mod(xodd + xpow,2) == 1) || (mod(yodd + ypow,2) == 1) 
+            rhs(i) = 0;
+        else
+            ftest = exp(-1/(25*h^2)*(X.^2+Y.^2)).*(X.^xpow).*(Y.^ypow);
+            integrand = @(x,y) intfun(x,y).*exp(-1/(25*h^2)*(x.^2+y.^2)).*(x.^xpow).*(y.^ypow);
+            ymax = min(30*h,ymax);
+            rhs(i) = integral2(integrand,-ymax,ymax,-ymax,ymax)/h^2 - sum(gmat.*ftest,'all');
+        end
+        
+        i = i + 1;
+    end
+    z = z + 1;
+end
+
+corrections = lhs \ rhs;
+
+ind = intersect(find(X == 0), find(Y == 0));
+[centx, centy] = ind2sub(size(X),ind);
+
+
+for i = 1:nt
+    gmat(iis(i)+centx,jjs(i)+centy) = gmat(iis(i)+centx,jjs(i)+centy) + corrections(i);
+end
+
+intmat = gmat*h^2;
+
+end
+
+
+function [x,y] = cantor(z)
+%
+% Gets Cantor pairing from a positive integer z
+%
+    w = floor((sqrt(8*z+1)-1)/2);
+    t = (w^2+w)/2;
+    y = z - t;
+    x = w-y;
+end
